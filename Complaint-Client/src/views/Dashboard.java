caa0fe7d32c7793a243bfd218de8377bf6029d77
dillon <dillon@LAPTOP-42LV5BPP>
2023-04-05 08:40:37 -0500

    Advisor Dashboard added and updated Student Dashboard updated Supervisor Dashboard added and updated

--- Complaint-Client/src/views/Dashboard.java
+++ Complaint-Client/src/views/Dashboard.java
@@ -13,7 +13,6 @@ import java.awt.Image;
 import javax.swing.ImageIcon;
 
 import controller.Client;
-import guirun.LiveChatGUI;
 import models.Complaint;
 import models.Query;
 import models.Student;
@@ -45,10 +44,17 @@ public class Dashboard extends JFrame {
       if (client.getStudent()!=null){
           this.renderStudentComponents();
       }
+      if (client.getSupervisor()!=null) {
+    	  this.renderSupervisorComponents();
+      }
+      if (client.getAdvisor()!=null) {
+    	  this.renderAdvisorComponents();
+      }
   }
 
     private void renderStudentComponents() {
-        setTitle("Student Dashboard");
+       
+    	setTitle("Student Dashboard");
         setResizable(true);
         setVisible(true);
         setSize(900, 600);
@@ -97,7 +103,7 @@ public class Dashboard extends JFrame {
         idLabel.setHorizontalAlignment(JLabel.RIGHT);
 
         // Create the name label
-        nameLabel = new JLabel("Name: " + client.getStudent().getFirstName());
+        nameLabel = new JLabel("Name: " + client.getStudent().getFirstName()+ client.getStudent().getLastName());
         desktop.add(nameLabel);
         nameLabel.setBounds(600, 80, 200, 20);
         nameLabel.setHorizontalAlignment(JLabel.RIGHT);
@@ -179,7 +185,246 @@ public class Dashboard extends JFrame {
         // Display the window
         setVisible(true);
     }
+    
+    private void renderSupervisorComponents() {
+    	//Student Services Supervisor Dashboard
+    	
+    	        setTitle("Student Services Supervisor Dashboard");
+    	        setResizable(true);
+    	        setVisible(true);
+    	        setSize(900, 600);
+    	        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	        setLocationRelativeTo(null);
+    	        
 
+    	        // Create the menu bar
+    	        menuBar = new JMenuBar();
+    	        fileMenu = new JMenu("Exit Dashboard");
+    	        exitMenuItem = new JMenuItem("Exit");
 
+    	        exitMenuItem.addActionListener(new ActionListener() {
+    	            public void actionPerformed(ActionEvent e) {
+    	                System.exit(0);
+    	            }
+    	        });
+
+    	        fileMenu.add(exitMenuItem);
+    	        menuBar.add(fileMenu);
+    	        setJMenuBar(menuBar);
+
+    	        // Create the desktop pane
+    	        desktop = new JDesktopPane();
+    	        setContentPane(desktop);
+    	        
+    	        
+    	        
+    	        ImageIcon profilePictureIcon = new ImageIcon("C:\\Users\\dillon\\Downloads\\user.png");
+    	        Image profilePictureImage = profilePictureIcon.getImage().getScaledInstance(40, 40, Image.SCALE_SMOOTH);
+    	        ImageIcon profilePicture = new ImageIcon(profilePictureImage);
+    	        profilePictureLabel = new JLabel();
+    	        profilePictureLabel.setIcon(profilePicture);
+    	        desktop.add(profilePictureLabel);
+    	        profilePictureLabel.setBounds(750, 20, 50, 40);
+    	        
+
+    	     // Create the welcome label
+    	        welcomeLabel = new JLabel("Welcome, " + client.getSupervisor().getFirstName() + "!");
+    	        welcomeLabel.setFont(new Font("Arial", Font.BOLD, 20));
+    	        desktop.add(welcomeLabel);
+    	        welcomeLabel.setBounds(20, 20, 300, 30);
+
+    	        // Create the ID label
+    	        idLabel = new JLabel("ID: " + client.getSupervisor().getIdNumber());
+    	        desktop.add(idLabel);
+    	        idLabel.setBounds(600, 60, 200, 20);
+    	        idLabel.setHorizontalAlignment(JLabel.RIGHT);
+
+    	        // Create the name label
+    	        nameLabel = new JLabel("Name: " + client.getSupervisor().getFirstName()+ client.getSupervisor().getLastName());
+    	        desktop.add(nameLabel);
+    	        nameLabel.setBounds(600, 80, 200, 20);
+    	        nameLabel.setHorizontalAlignment(JLabel.RIGHT);
+
+    	        // Create the buttons
+    	        createButton = new JButton("View Outstanding Complaints/Queries");
+    	        desktop.add(createButton);
+    	        createButton.setBounds(20, 120, 200, 30);
+    	        createButton.addActionListener(new ActionListener() {
+    	            public void actionPerformed(ActionEvent e) {
+    	                // Code to handle view button click
+    	            }
+    	        });
+
+    	        viewButton = new JButton("View Resolved Complaints/Queries");
+    	        desktop.add(viewButton);
+    	        viewButton.setBounds(20, 160, 200, 30);
+    	        viewButton.addActionListener(new ActionListener() {
+    	            public void actionPerformed(ActionEvent e) {
+    	                // Code to handle view button click
+    	            }
+    	        });
+    	        
+    	        viewButton = new JButton("Assign Complaints/Queries");
+    	        desktop.add(viewButton);
+    	        viewButton.setBounds(20, 200, 200, 30);
+    	        viewButton.addActionListener(new ActionListener() {
+    	            public void actionPerformed(ActionEvent e) {
+    	                // Code to handle Assign button click
+    	            }
+    	        });
+    	        
+    	        viewButton = new JButton("View All Complaints/Queries");
+    	        desktop.add(viewButton);
+    	        viewButton.setBounds(20, 240, 200, 30);
+    	        viewButton.addActionListener(new ActionListener() {
+    	            public void actionPerformed(ActionEvent e) {
+    	                // Code to handle view button click
+    	            }
+    	        });
+    	        
+    	        
+    	        //notifications button
+    	        notificationButton = new JButton();
+    	        ImageIcon notifIcon = new ImageIcon("C:\\Users\\dillon\\Downloads\\bell-ring.png");
+    	        Image notifImage = notifIcon.getImage().getScaledInstance(35, 35, Image.SCALE_SMOOTH);
+    	        notificationButton.setIcon(new ImageIcon(notifImage));
+    	        desktop.add(notificationButton);
+    	        notificationButton.setBounds(670, 20, 50, 40);
+    	        notificationButton.addActionListener(new ActionListener() {
+    	            public void actionPerformed(ActionEvent e) {
+    	                // Code to handle chat button click
+    	            }
+    	        });
+    	       
+    	        
+    	        // Set desktop background color and add image
+    	       desktop.setBackground(new Color(245, 245, 245));
+    	        ImageIcon backgroundImage = new ImageIcon("C:\\Users\\dillon\\Downloads\\background.png");
+    	        JLabel backgroundLabel = new JLabel(backgroundImage);
+    	        backgroundLabel.setBounds(0, 0, 900, 600);
+    	        desktop.add(backgroundLabel); 
+
+    	        // Display the window
+    	        setVisible(true);
+    	    }
+    
+    private void renderAdvisorComponents() {
+    	    	
+    	        // Set up the window
+    	        setTitle("Student Dashboard");
+    	        setResizable(true);
+    	        setVisible(true);
+    	        setSize(900, 600);
+    	        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	        setLocationRelativeTo(null);
+    	        
+
+    	        // Create the menu bar
+    	        menuBar = new JMenuBar();
+    	        fileMenu = new JMenu("Exit Dashboard");
+    	        exitMenuItem = new JMenuItem("Exit");
+
+    	        exitMenuItem.addActionListener(new ActionListener() {
+    	            public void actionPerformed(ActionEvent e) {
+    	                System.exit(0);
+    	            }
+    	        });
+
+    	        fileMenu.add(exitMenuItem);
+    	        menuBar.add(fileMenu);
+    	        setJMenuBar(menuBar);
+
+    	        // Create the desktop pane
+    	        desktop = new JDesktopPane();
+    	        setContentPane(desktop);
+    	        
+    	        
+    	        
+    	        ImageIcon profilePictureIcon = new ImageIcon("C:\\Users\\dillon\\Downloads\\user.png");
+    	        Image profilePictureImage = profilePictureIcon.getImage().getScaledInstance(40, 40, Image.SCALE_SMOOTH);
+    	        ImageIcon profilePicture = new ImageIcon(profilePictureImage);
+    	        profilePictureLabel = new JLabel();
+    	        profilePictureLabel.setIcon(profilePicture);
+    	        desktop.add(profilePictureLabel);
+    	        profilePictureLabel.setBounds(750, 20, 50, 40);
+    	        
+
+    	        // Create the welcome label
+    	        welcomeLabel = new JLabel("Welcome, " + client.getAdvisor().getFirstName() + "!");
+    	        welcomeLabel.setFont(new Font("Arial", Font.BOLD, 20));
+    	        desktop.add(welcomeLabel);
+    	        welcomeLabel.setBounds(20, 20, 300, 30);
+
+    	        // Create the ID label
+    	        idLabel = new JLabel("ID: " + client.getAdvisor().getIdNumber());
+    	        desktop.add(idLabel);
+    	        idLabel.setBounds(600, 60, 200, 20);
+    	        idLabel.setHorizontalAlignment(JLabel.RIGHT);
+
+    	        // Create the name label
+    	        nameLabel = new JLabel("Name: " + client.getAdvisor().getFirstName() + client.getAdvisor().getLastName());
+    	        desktop.add(nameLabel);
+    	        nameLabel.setBounds(600, 80, 200, 20);
+    	        nameLabel.setHorizontalAlignment(JLabel.RIGHT);
+
+    	        // Create the buttons
+
+    	        viewButton = new JButton("View Assigned Query/Complaint");
+    	        desktop.add(viewButton);
+    	        viewButton.setBounds(20, 120, 200, 30);
+    	        viewButton.addActionListener(new ActionListener() {
+    	            public void actionPerformed(ActionEvent e) {
+    	                // Code to handle view button click
+    	            }
+    	        });
+    	        
+    	        viewButton = new JButton("View All Query/Complaint");
+    	        desktop.add(viewButton);
+    	        viewButton.setBounds(20, 160, 200, 30);
+    	        viewButton.addActionListener(new ActionListener() {
+    	            public void actionPerformed(ActionEvent e) {
+    	                // Code to handle view button click
+    	            }
+    	        });
+    	        
+    	        chatButton = new JButton();
+    	        ImageIcon chatIcon = new ImageIcon("C:\\Users\\dillon\\Downloads\\chat.png");
+    	        Image chatImage = chatIcon.getImage().getScaledInstance(35, 35, Image.SCALE_SMOOTH);
+    	        chatButton.setIcon(new ImageIcon(chatImage));
+    	        desktop.add(chatButton);
+    	        chatButton.setBounds(640, 20, 50, 40);
+    	        chatButton.addActionListener(new ActionListener() {
+    	            public void actionPerformed(ActionEvent e) {
+    	                // Code to handle chat button click
+    	            	 desktop.add(new LiveChatGUI(client));
+    	            }
+    	        });
+    	        
+    	        //notifications button
+    	        notificationButton = new JButton();
+    	        ImageIcon notifIcon = new ImageIcon("C:\\Users\\dillon\\Downloads\\bell-ring.png");
+    	        Image notifImage = notifIcon.getImage().getScaledInstance(35, 35, Image.SCALE_SMOOTH);
+    	        notificationButton.setIcon(new ImageIcon(notifImage));
+    	        desktop.add(notificationButton);
+    	        notificationButton.setBounds(570, 20, 50, 40);
+    	        notificationButton.addActionListener(new ActionListener() {
+    	            public void actionPerformed(ActionEvent e) {
+    	                // Code to handle chat button click
+    	            }
+    	        });
+    	       
+    	        
+    	        // Set desktop background color and add image
+    	       desktop.setBackground(new Color(245, 245, 245));
+    	        ImageIcon backgroundImage = new ImageIcon("C:\\Users\\dillon\\Downloads\\background.png");
+    	        JLabel backgroundLabel = new JLabel(backgroundImage);
+    	        backgroundLabel.setBounds(0, 0, 900, 600);
+    	        desktop.add(backgroundLabel); 
+
+    	        // Display the window
+    	        setVisible(true);
+    	    }
+
+    	 
 }  
 
